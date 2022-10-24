ad15fd09a43c84a3c5cd2dd46418c5f980e4a3fa
taite <taiengchan@gmail.com>
2022-10-24 17:00:46 +0800

    fixed home UI

--- app/src/main/java/com/example/rabbitu/MainActivity.java
+++ app/src/main/java/com/example/rabbitu/MainActivity.java
@@ -1,21 +1,57 @@
 package com.example.rabbitu;
 
 import android.os.Bundle;
-import android.view.View;
+import android.view.MenuItem;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import com.example.rabbitu.databinding.ActivityMainBinding;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.android.material.navigation.NavigationBarView;
 
 public class MainActivity extends AppCompatActivity {
 
-    private ActivityMainBinding binding;
+    private static final String TAG = "MainActivity";
+
+    BottomNavigationView mBottomNavigationView;
+
+    HomeFragment mHomeFragment = new HomeFragment();
+    BookFragment mBookFragment = new BookFragment();
+    LeaderboardFragment mLeaderboardFragment = new LeaderboardFragment();
+    NotesFragment mNotesFragment = new NotesFragment();
+    SettingsFragment mSettingsFragment = new SettingsFragment();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        binding = ActivityMainBinding.inflate(getLayoutInflater());
-        View v = binding.getRoot();
-        setContentView(v);
+        setContentView(R.layout.activity_main);
+
+        mBottomNavigationView = findViewById(R.id.bottom_navigation_bar);
+
+        getSupportFragmentManager().beginTransaction().replace(R.id.fragmentContainerView, mHomeFragment).commit();
+
+        mBottomNavigationView.setOnItemSelectedListener(new NavigationBarView.OnItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(MenuItem item) {
+                switch(item.getItemId()){
+                    case R.id.item1:
+                        getSupportFragmentManager().beginTransaction().setCustomAnimations(android.R.anim.fade_in, android.R.anim.fade_out).replace(R.id.fragmentContainerView, mHomeFragment).commit();
+                        return true;
+                    case R.id.item2:
+                        getSupportFragmentManager().beginTransaction().setCustomAnimations(android.R.anim.fade_in, android.R.anim.fade_out).replace(R.id.fragmentContainerView, mBookFragment).commit();
+                        return true;
+                    case R.id.item3:
+                        getSupportFragmentManager().beginTransaction().setCustomAnimations(android.R.anim.fade_in, android.R.anim.fade_out).replace(R.id.fragmentContainerView, mLeaderboardFragment).commit();
+                        return true;
+                    case R.id.item4:
+                        getSupportFragmentManager().beginTransaction().setCustomAnimations(android.R.anim.fade_in, android.R.anim.fade_out).replace(R.id.fragmentContainerView, mNotesFragment).commit();
+                        return true;
+                    case R.id.item5:
+                        getSupportFragmentManager().beginTransaction().setCustomAnimations(android.R.anim.fade_in, android.R.anim.fade_out).replace(R.id.fragmentContainerView, mSettingsFragment).commit();
+                        return true;
+                }
+
+                return false;
+            }
+        });
     }
 }
