ffb7e935aa8001d331ee1cd82a3bcfd9eaecf371
taite <taiengchan@gmail.com>
2022-11-21 13:24:19 +0800

    done add and delete song

---
+++ app/src/main/java/com/example/rabbitu/AdminEditMusic.java
@@ -0,0 +1,13 @@
+package com.example.rabbitu;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class AdminEditMusic extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_admin_edit_music);
+    }
+}
