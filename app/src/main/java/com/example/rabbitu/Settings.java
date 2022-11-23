8defef5584cdcdb5ac7635dc06ed5896ef59fb59
taite <taiengchan@gmail.com>
2022-11-24 01:46:07 +0800

    Temp success

--- app/src/main/java/com/example/rabbitu/Settings.java
+++ app/src/main/java/com/example/rabbitu/Settings.java
@@ -1,6 +1,7 @@
 package com.example.rabbitu;
 
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
