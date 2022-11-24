b05ce37102118e496c43889ddab846e543888f64
taite <taiengchan@gmail.com>
2022-11-24 13:41:47 +0800

    done admin logout

--- app/src/main/java/com/example/rabbitu/Admin.java
+++ app/src/main/java/com/example/rabbitu/Admin.java
@@ -3,22 +3,16 @@ package com.example.rabbitu;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.Button;
-import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.google.firebase.auth.FirebaseAuth;
-import com.google.firebase.auth.FirebaseUser;
-import com.google.firebase.database.DatabaseReference;
-import com.google.firebase.database.FirebaseDatabase;
-
-import org.w3c.dom.Text;
 
 public class Admin extends AppCompatActivity {
 
-    FloatingActionButton editPopUp, bookBtn, musicBtn;
+    FirebaseAuth mAuth;
+    FloatingActionButton editPopUp, bookBtn, musicBtn, logoutBtn;
     public boolean isMenuOpen = false;
 
     @Override
@@ -26,9 +20,12 @@ public class Admin extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_admin);
 
+        mAuth = FirebaseAuth.getInstance();
+
         editPopUp = findViewById(R.id.edit_popup);
         bookBtn = findViewById(R.id.btn_book);
         musicBtn = findViewById(R.id.btn_music);
+        logoutBtn = findViewById(R.id.btn_logout);
 
         editPopUp.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -46,11 +43,18 @@ public class Admin extends AppCompatActivity {
                 startActivity(new Intent(Admin.this,AdminEditBook.class));
             }
         });
-
         musicBtn.setOnClickListener(new View.OnClickListener(){
             @Override
             public void onClick(View view) {
                 startActivity(new Intent(Admin.this,AdminMusic.class));
+            }
+        });
+        logoutBtn.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view) {
+                mAuth.signOut();
+                startActivity(new Intent(Admin.this,Login.class));
+                finish();
             }
         });
     }
@@ -59,11 +63,13 @@ public class Admin extends AppCompatActivity {
         isMenuOpen = true;
         musicBtn.animate().translationY(-getResources().getDimension(R.dimen.standard_65));
         bookBtn.animate().translationY(-getResources().getDimension(R.dimen.standard_120));
+        logoutBtn.animate().translationY(-getResources().getDimension(R.dimen.standard_175));
     }
 
     private void closeFABMenu(){
         isMenuOpen = false;
         musicBtn.animate().translationY(0);
         bookBtn.animate().translationY(0);
+        logoutBtn.animate().translationY(0);
     }
 }
